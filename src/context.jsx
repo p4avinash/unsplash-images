386ed2d662a19a952b94a6e24c326c90299b7523
p4avinash <p4avinashkumar@gmail.com>
2023-09-24 13:00:12 +0530

    added user preference theme, search functionality

--- src/context.jsx
+++ src/context.jsx
@@ -2,18 +2,32 @@ import { createContext, useContext, useState, useEffect } from "react"
 
 const AppContext = createContext()
 
+const getInitialDarkMode = () => {
+  const storedDarkMode = localStorage.getItem("darkTheme") === "true"
+  return storedDarkMode
+}
+
 export const AppProvider = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false)
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode())
+  const [searchTerm, setSearchTerm] = useState("cat")
 
   const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme
     setIsDarkTheme(newDarkTheme)
     const body = document.querySelector("body")
     body.classList.toggle("dark-theme", newDarkTheme)
+    localStorage.setItem("darkTheme", newDarkTheme)
   }
 
+  useEffect(() => {
+    const body = document.querySelector("body")
+    body.classList.toggle("dark-theme", isDarkTheme)
+  }, [])
+
   return (
-    <AppContext.Provider value={{ isDarkTheme, toggleDarkTheme }}>
+    <AppContext.Provider
+      value={{ isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm }}
+    >
       {children}
     </AppContext.Provider>
   )
